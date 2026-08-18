b73d973679f3ebba41ebe2f7e34a940a8c53bfac
wangxingkang <wang_xingkang@qq.com>
2026-08-18 16:11:53 +0800

    chore: configure web app workspace

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,14 +1,19 @@
 import tailwindcss from '@tailwindcss/vite';
 import { devtools } from '@tanstack/devtools-vite';
-
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
-
 import viteReact from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 const config = defineConfig({
-  resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [
+    devtools(),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
 });
 
 export default config;
